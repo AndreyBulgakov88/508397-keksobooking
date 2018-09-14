bd3b0d8be7559d7a52bf19923739fadfb84cdd5c
Andrey Bulgakov <andreybulgakov88@gmail.com>
2018-09-15 01:21:40 +0700

    активация страницы, заполнение адреса основной метки, просмотр похожих объявлений

--- js/map.js
+++ js/map.js
@@ -23,16 +23,29 @@ var HOTEL_PHOTOS = [
 var CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
 var CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
 var ADVERTISEMENTS_COUNT = 8;
+
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
 var MAP_WIDTH = 1200;
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
+var PIN_MAIN_WIDTH = 62;
+var PIN_MAIN_HEIGHT = 62;
+var PIN_MAIN_ARROW_HEIGHT = 22;
 
 var mapSection = document.querySelector('.map');
 var mapPins = document.querySelector('.map__pins');
+var mapPinMain = document.querySelector('.map__pin--main');
+
+var advertisementForm = document.querySelector('.ad-form');
+var addressInput = document.querySelector('input[name=address]');
+
 var pinTemplate = document.querySelector('#pin').content;
 var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 
 
+// functions for working with numbers and arrays
 var getRandomNumber = function (minValue, maxValue) {
   return Math.floor((maxValue + 1 - minValue) * Math.random() + minValue);
 };
@@ -59,21 +72,8 @@ function shuffleArray(initialArray) {
   return resultArray;
 }
 
-var generateAdvertisements = function () {
-  var advertisementsArray = [];
 
-  for (var i = 0; i < ADVERTISEMENTS_COUNT; i++) {
-    var advertisement = {};
-    advertisement.author = generateAdvertisementAutor(i + 1);
-    advertisement.location = generateAdvertisementLocation();
-    advertisement.offer = generateAdvertisementOffer(i + 1, advertisement.location);
-
-    advertisementsArray.push(advertisement);
-  }
-
-  return advertisementsArray;
-};
-
+// functions for generating advertisements
 var generateAdvertisementAutor = function (advertisementNumber) {
   var author = {};
   author.avatar = 'img/avatars/user0' + advertisementNumber + '.png';
@@ -83,17 +83,10 @@ var generateAdvertisementAutor = function (advertisementNumber) {
 
 var generateAdvertisementLocation = function () {
   var location = {};
-  location.x = getRandomNumber(0, MAP_WIDTH) - PIN_WIDTH / 2;
-  location.y = getRandomNumber(130, 630) - PIN_HEIGHT;
+  location.x = getRandomNumber(0, MAP_WIDTH);
+  location.y = getRandomNumber(130, 630);
 
   return location;
-};
-
-var generateHotelFeatures = function () {
-  var featuresCount = getRandomNumber(0, HOTEL_FEATURES.length);
-  var features = getRandomValuesFromArray(HOTEL_FEATURES, featuresCount);
-
-  return features;
 };
 
 var generateAdvertisementOffer = function (advertisementNumber, advertisementLocation) {
@@ -114,22 +107,55 @@ var generateAdvertisementOffer = function (advertisementNumber, advertisementLoc
   return offer;
 };
 
-var renderArrayToChildNodes = function (features, callback) {
+var generateHotelFeatures = function () {
+  var featuresCount = getRandomNumber(0, HOTEL_FEATURES.length);
+  var features = getRandomValuesFromArray(HOTEL_FEATURES, featuresCount);
+
+  return features;
+};
+
+var generateAdvertisements = function () {
+  var advertisementsArray = [];
+
+  for (var i = 0; i < ADVERTISEMENTS_COUNT; i++) {
+    var advertisement = {};
+    advertisement.author = generateAdvertisementAutor(i + 1);
+    advertisement.location = generateAdvertisementLocation();
+    advertisement.offer = generateAdvertisementOffer(i + 1, advertisement.location);
+
+    advertisementsArray.push(advertisement);
+  }
+
+  return advertisementsArray;
+};
+
+
+// functions for working with child nodes
+var renderArrayToChildNodes = function (array, callback) {
   var fragment = document.createDocumentFragment();
-  features.forEach(function (element) {
-    fragment.appendChild(callback(element));
+  array.forEach(function (element, index) {
+    fragment.appendChild(callback(element, index));
   });
 
   return fragment;
 };
 
-var renderPin = function (advertisement) {
+var removeChildNodes = function (parentNode) {
+  while (parentNode.lastChild) {
+    parentNode.removeChild(parentNode.lastChild);
+  }
+};
+
+
+// functions for rendering elements
+var renderPin = function (advertisement, index) {
   var pinElement = pinTemplate.cloneNode(true);
 
-  pinElement.querySelector('.map__pin').style.left = advertisement.location.x + 'px';
-  pinElement.querySelector('.map__pin').style.top = advertisement.location.y + 'px';
+  pinElement.querySelector('.map__pin').style.left = advertisement.location.x - PIN_WIDTH / 2 + 'px';
+  pinElement.querySelector('.map__pin').style.top = advertisement.location.y - PIN_HEIGHT + 'px';
   pinElement.querySelector('.map__pin').children[0].src = advertisement.author.avatar;
   pinElement.querySelector('.map__pin').children[0].alt = advertisement.offer.title;
+  pinElement.querySelector('.map__pin').setAttribute('data-id', index);
 
   return pinElement;
 };
@@ -151,12 +177,6 @@ var makePhotoElement = function (photo) {
   photoElement.alt = 'Фотография жилья';
 
   return photoElement;
-};
-
-var removeChildNodes = function (parentNode) {
-  while (parentNode.lastChild) {
-    parentNode.removeChild(parentNode.lastChild);
-  }
 };
 
 var renderCard = function (advertisement) {
@@ -183,9 +203,116 @@ var renderCard = function (advertisement) {
   return cardElement;
 };
 
-mapSection.classList.remove('map--faded');
 
-var advertisementsArray = generateAdvertisements();
+// functions for working with advertisements popup
+var getAdvertisementId = function (element) {
+  if (!element.classList.contains('map__pin')) {
+    return element.parentNode.getAttribute('data-id');
+  } else {
+    return element.getAttribute('data-id');
+  }
+};
 
-mapPins.appendChild(renderArrayToChildNodes(advertisementsArray, renderPin));
-mapSection.insertBefore(renderCard(advertisementsArray[0]), document.querySelector('.map__filters-container'));
+var openAdvertisementPopup = function (advertisement) {
+  var currentAdvertisement = document.querySelector('.popup');
+
+  if (!currentAdvertisement) {
+    currentAdvertisement = renderCard(advertisement);
+    mapSection.insertBefore(currentAdvertisement, document.querySelector('.map__filters-container'));
+  } else {
+    var newAdvertisement = renderCard(advertisement);
+    mapSection.replaceChild(newAdvertisement, currentAdvertisement);
+    currentAdvertisement = newAdvertisement;
+  }
+};
+
+var thisIsPin = function (element) {
+  if ((!element.classList.contains('map__pin') && !element.parentNode.classList.contains('map__pin'))
+       || element.classList.contains('map__pin--main')
+       || element.parentNode.classList.contains('map__pin--main')) {
+    return false;
+  }
+
+  return true;
+};
+
+
+// generating advertisements, adding listeners to advertisements
+var configureAdvertisements = function () {
+  var advertisementsArray = generateAdvertisements();
+  mapPins.appendChild(renderArrayToChildNodes(advertisementsArray, renderPin));
+
+  mapPins.addEventListener('click', function (evt) {
+    if (!thisIsPin(evt.target)) {
+      return;
+    }
+
+    openAdvertisementPopup(advertisementsArray[getAdvertisementId(evt.target)]);
+
+    addPopupCloseListeners();
+  });
+};
+
+
+// popup handlers
+var popupEscPressHandler = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var closePopup = function () {
+  mapSection.removeChild(document.querySelector('.popup'));
+  document.removeEventListener('keydown', popupEscPressHandler);
+};
+
+var addPopupCloseListeners = function () {
+  document.addEventListener('keydown', popupEscPressHandler);
+
+  var popupClose = document.querySelector('.popup__close');
+  popupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  popupClose.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      closePopup();
+    }
+  });
+};
+
+
+// page activation handler
+var mapPinMainMouseUpHandler = function () {
+  mapSection.classList.remove('map--faded');
+  advertisementForm.classList.remove('ad-form--disabled');
+
+  var disabledElements = document.querySelectorAll('[disabled]');
+  disabledElements.forEach(function (element) {
+    element.disabled = false;
+  });
+
+  configureAdvertisements();
+
+  addressInput.value = getPinMainLocation(true);
+
+  mapPinMain.removeEventListener('mouseup', mapPinMainMouseUpHandler);
+};
+
+
+// getting main pin location
+var getPinMainLocation = function (arrowActive) {
+  var locationX = parseInt(mapPinMain.style.left, 10) + PIN_MAIN_WIDTH / 2;
+  var locationY = parseInt(mapPinMain.style.top, 10) + PIN_MAIN_HEIGHT / 2;
+
+  if (arrowActive) {
+    locationY = locationY + PIN_MAIN_HEIGHT / 2 + PIN_MAIN_ARROW_HEIGHT;
+  }
+
+  return locationX + ', ' + locationY;
+};
+
+
+// initial settings
+addressInput.value = getPinMainLocation();
+mapPinMain.addEventListener('mouseup', mapPinMainMouseUpHandler);
