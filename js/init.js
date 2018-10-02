460cd0021622279aca7f2fce571d26b4f30e919d
Andrey Bulgakov <andreybulgakov88@gmail.com>
2018-10-03 00:00:12 +0700

    исправление замечаний (2) после первой финальной проверки проекта

--- js/init.js
+++ js/init.js
@@ -64,7 +64,7 @@
       var mapPinMainCoordY = mapPinMainElement.offsetTop - shift.y;
 
       mapPinMainCoordX = Math.max(window.map.PIN_STARTING_COORD_X - window.map.PIN_MAIN_WIDTH / 2, mapPinMainCoordX);
-      mapPinMainCoordX = Math.min(window.map.PIN_ENDING_COORD_X - window.map.PIN_MAIN_WIDTH / 2, mapPinMainCoordX);
+      mapPinMainCoordX = Math.min(mapSectionElement.clientWidth - window.map.PIN_MAIN_WIDTH / 2, mapPinMainCoordX);
       mapPinMainCoordY = Math.max(window.map.PIN_STARTING_COORD_Y - (window.map.PIN_MAIN_HEIGHT + window.map.PIN_MAIN_ARROW_HEIGHT), mapPinMainCoordY);
       mapPinMainCoordY = Math.min(window.map.PIN_ENDING_COORD_Y - (window.map.PIN_MAIN_HEIGHT + window.map.PIN_MAIN_ARROW_HEIGHT), mapPinMainCoordY);
 
